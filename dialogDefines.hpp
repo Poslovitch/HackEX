4bba217ba0a6365a1c1a6f97e94406ec8523c301
Florian CUNY <poslovitch@bentobox.world>
2021-04-11 13:41:19 +0200

    Added a dialogDefines file to define all dialog stuff

---
+++ dialogDefines.hpp
@@ -0,0 +1,6 @@
+#define COLOR_GREEN {0.3, 0.6, 0.4, 0.9}
+#define COLOR_SEE_THROUGH_BLACK { 0, 0, 0, 0.9 }
+#define COLOR_SOLID_GRAY { 0.3, 0.3, 0.3, 0.98 }
+#define COLOR_TRANSPARENT { 0, 0, 0, 0 }
+#define ST_MULTI 16
+#define CT_STATIC 0
--- terminal/dialog.hpp
+++ terminal/dialog.hpp
@@ -1,9 +1,4 @@
-#define COLOR_GREEN {0.3, 0.6, 0.4, 0.9}
-#define COLOR_SEE_THROUGH_BLACK { 0, 0, 0, 0.9 }
-#define COLOR_SOLID_GRAY { 0.3, 0.3, 0.3, 0.98 }
-#define COLOR_TRANSPARENT { 0, 0, 0, 0 }
-#define ST_MULTI 16
-#define CT_STATIC 0
+#include "dialogDefines.hpp"
 
 class Terminal {
   idd = 42001;
